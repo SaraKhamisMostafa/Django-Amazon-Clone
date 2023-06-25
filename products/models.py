29a6329550af9989c37a077f799958f6fd6d6ed5
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-06-25 13:07:34 +0300

    add frontend work

--- products/models.py
+++ products/models.py
@@ -14,6 +14,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ProductImages(models.Model):
+    product= models.ForeignKey(Product,related_name='product_image',on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='product_image')
+
+    def __str__(self):
+        return str(self.product)
+
 
 
 
